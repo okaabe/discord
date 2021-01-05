b2b9fe324f12107408a7a55c9b43383d34650751
okaabe <orlandi2006@hotmail.com>
2021-01-04 23:33:41 -0300

    Added the mongodb connection

--- src/server.js
+++ src/server.js
@@ -1,5 +1,20 @@
 require('dotenv/config');
 
+const { connect } = require('mongoose');
 const app = require('./app');
 
-app.login(process.env.DISCORD_TOKEN);
+
+connect(process.env.MONGODB_URIS, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+}, (err) => {
+    if (err) {
+        throw err;
+    }
+
+    console.log('Database connected');
+    app.login(process.env.DISCORD_TOKEN).then(() => {
+        console.log('Discord connected');
+    });
+});
